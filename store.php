eaa57568a38109f961146f3f475f84f881c6931c
sangenx09 <santhoshravi23092002@gmail.com>
2025-09-14 21:55:40 +0530

    Delete store.php

--- store.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$myfile = fopen("location.txt", "w");
-$txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"]. "\nIP: " . $_SERVER["REMOTE_ADDR"] . "\nUser agent 
-    :" . $_GET["uagent"];
-fwrite($myfile, $txt);
-fclose($myfile);
-?>
